5d5c70b2c2a2f32ca05386b5b65a36609236b60c
cyanidepanda <cyanide@panda-lang.org>
2015-11-07 19:49:30 +0100

    1.0.0-SNAPSHOT

--- src/main/java/org/panda_lang/panda/core/parser/util/Exec.java
+++ src/main/java/org/panda_lang/panda/core/parser/util/Exec.java
@@ -11,18 +11,18 @@ public class Exec {
     public static void activate(String source) {
         try {
             if (source == null || source.isEmpty()) return;
-            String[] parts = source.split(":~>~:", 2);
+            String[] parts = source.split("=", 2);
 
             if (parts.length < 1 || parts[0] == null) return;
             String command = parts[0].toUpperCase();
 
             switch (command) {
-                case "FILE":
+                case "-FILE":
                     PandaScript fScript = PandaLoader.loadSimpleScript(new File(parts[1]));
                     if(fScript == null) return;
                     fScript.call(MethodBlock.class, "main");
                     break;
-                case "SOURCE":
+                case "-SOURCE":
                     PandaScript sScript = PandaLoader.loadSimpleScript(parts[1]);
                     if(sScript == null) return;
                     sScript.call(MethodBlock.class, "main");
