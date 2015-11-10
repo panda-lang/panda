e5366e62fa6132f2149e3173e3845b293df06383
cyanidepanda <cyanide@panda-lang.org>
2015-11-10 22:58:26 +0100

    1.0.0-SNAPSHOT

---
+++ src/main/java/org/panda_lang/panda/lang/net/PServerSocket.java
@@ -0,0 +1,17 @@
+package org.panda_lang.panda.lang.net;
+
+import org.panda_lang.panda.lang.PObject;
+
+public class PServerSocket extends PObject {
+
+    private final PServerSocket serverSocket;
+
+    public PServerSocket(PServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public PServerSocket getServerSocket() {
+        return serverSocket;
+    }
+
+}
---
+++ src/main/java/org/panda_lang/panda/lang/net/PSocket.java
@@ -0,0 +1,19 @@
+package org.panda_lang.panda.lang.net;
+
+import org.panda_lang.panda.lang.PObject;
+
+import java.net.Socket;
+
+public class PSocket extends PObject {
+
+    private final Socket socket;
+
+    public PSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+}
