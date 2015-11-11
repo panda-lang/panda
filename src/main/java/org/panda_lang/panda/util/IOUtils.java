482fc4b081e7db6f320da7be940601515547fbee
cyanidepanda <cyanide@panda-lang.org>
2015-11-11 23:07:55 +0100

    1.0.0-SNAPSHOT

--- src/main/java/org/panda_lang/panda/util/IOUtils.java
+++ src/main/java/org/panda_lang/panda/util/IOUtils.java
@@ -75,4 +75,9 @@ public class IOUtils {
         return s.replace("\t", "");
     }
 
+    public static String convertStreamToString(java.io.InputStream is) {
+        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+
 }
