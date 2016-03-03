eefc62ca8238144097364a2fad27e93a1fc64597
dzikoysk <dzikoysk@dzikoysk.net>
2016-03-03 23:16:23 +0100

    Added StringUtils.

---
+++ src/main/java/org/panda_lang/panda/util/StringUtils.java
@@ -0,0 +1,37 @@
+package org.panda_lang.panda.util;
+
+public class StringUtils {
+
+    public static String replace(String text, String searchString, String replacement) {
+        if (text == null || text.isEmpty() || searchString.isEmpty()) {
+            return text;
+        }
+        if (replacement == null) {
+            replacement = "";
+        }
+
+        int start = 0;
+        int max = -1;
+        int end = text.indexOf(searchString, start);
+        if (end == -1) {
+            return text;
+        }
+
+        int replaceLength = searchString.length();
+        int increase = replacement.length() - replaceLength;
+        increase = (increase < 0 ? 0 : increase);
+        increase *= 16;
+        StringBuilder sb = new StringBuilder(text.length() + increase);
+        while (end != -1) {
+            sb.append(text.substring(start, end)).append(replacement);
+            start = end + replaceLength;
+            if (--max == 0) {
+                break;
+            }
+            end = text.indexOf(searchString, start);
+        }
+        sb.append(text.substring(start));
+        return sb.toString();
+    }
+
+}
