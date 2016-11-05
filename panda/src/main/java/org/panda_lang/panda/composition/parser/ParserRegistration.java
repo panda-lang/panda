251ce4d4591c0374752c9bf69f8e0609c373f1bf
dzikoysk <dzikoysk@dzikoysk.net>
2016-11-05 16:12:47 +0100

    Fixed automatic registration by annotations

--- panda/src/main/java/org/panda_lang/panda/composition/parser/ParserRegistration.java
+++ panda/src/main/java/org/panda_lang/panda/composition/parser/ParserRegistration.java
@@ -3,6 +3,14 @@ package org.panda_lang.panda.composition.parser;
 import org.panda_lang.core.interpreter.parser.Parser;
 import org.panda_lang.core.interpreter.parser.ParserHandler;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+
 public @interface ParserRegistration {
 
     Class<? extends Parser> parserClass();
@@ -10,4 +18,5 @@ public @interface ParserRegistration {
     Class<? extends ParserHandler> handlerClass();
 
 }
+
 
