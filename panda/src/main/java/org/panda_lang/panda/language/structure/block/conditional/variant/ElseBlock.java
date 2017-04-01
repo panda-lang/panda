98991409125fec9740c232d57cfbeb66f31ce40b
dzikoysk <dzikoysk@dzikoysk.net>
2017-04-01 12:34:50 +0200

    Add ConditionalBlockParser and else statement

---
+++ panda/src/main/java/org/panda_lang/panda/language/structure/block/conditional/variant/ElseBlock.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015-2017 Dzikoysk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.panda_lang.panda.language.structure.block.conditional.variant;
+
+import org.panda_lang.panda.implementation.structure.dynamic.Block;
+import org.panda_lang.panda.implementation.structure.util.AbstractContainer;
+import org.panda_lang.panda.implementation.structure.util.ExecutableBridgeUtils;
+import org.panda_lang.panda.language.runtime.ExecutableBridge;
+
+public class ElseBlock extends AbstractContainer implements Block {
+
+    @Override
+    public void execute(ExecutableBridge bridge) {
+        ExecutableBridgeUtils.execute(bridge, getStatementCells());
+    }
+
+}
