dee1e001d6f763ff6c0d461d2c55c0bdb858a8f8
dzikoysk <dzikoysk@dzikoysk.net>
2018-01-23 00:40:40 +0100

    Part of the vague extractor and math expression

---
+++ panda/src/main/java/org/panda_lang/panda/language/structure/general/expression/callbacks/math/MathExpressionCallback.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2015-2018 Dzikoysk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.panda_lang.panda.language.structure.general.expression.callbacks.math;
+
+import org.panda_lang.panda.core.structure.value.Value;
+import org.panda_lang.panda.language.runtime.ExecutableBranch;
+import org.panda_lang.panda.language.structure.general.expression.Expression;
+import org.panda_lang.panda.language.structure.general.expression.ExpressionCallback;
+
+public class MathExpressionCallback implements ExpressionCallback {
+
+    @Override
+    public Value call(Expression expression, ExecutableBranch branch) {
+        return null;
+    }
+
+}
--- panda/src/main/java/org/panda_lang/panda/language/structure/general/expression/callbacks/math/MathParser.java
+++ panda/src/main/java/org/panda_lang/panda/language/structure/general/expression/callbacks/math/MathParser.java
@@ -1,5 +1,23 @@
 package org.panda_lang.panda.language.structure.general.expression.callbacks.math;
 
-public class MathParser {
+import org.panda_lang.panda.core.interpreter.lexer.extractor.vague.VagueExtractor;
+import org.panda_lang.panda.core.interpreter.lexer.extractor.vague.VagueResult;
+import org.panda_lang.panda.framework.implementation.interpreter.token.reader.PandaTokenReader;
+import org.panda_lang.panda.framework.language.interpreter.parser.Parser;
+import org.panda_lang.panda.framework.language.interpreter.parser.ParserInfo;
+import org.panda_lang.panda.framework.language.interpreter.token.TokenizedSource;
+import org.panda_lang.panda.framework.language.interpreter.token.reader.TokenReader;
+import org.panda_lang.panda.language.syntax.tokens.Operators;
+
+public class MathParser implements Parser {
+
+    private static final VagueExtractor EXTRACTOR = new VagueExtractor(Operators.ADDITION, Operators.SUBTRACTION, Operators.DIVISION, Operators.MULTIPLICATION);
+
+    public MathExpression parse(TokenizedSource source, ParserInfo info) {
+        TokenReader reader = new PandaTokenReader(source);
+        VagueResult result = EXTRACTOR.extract(reader);
+
+        return new MathExpression();
+    }
 
 }
