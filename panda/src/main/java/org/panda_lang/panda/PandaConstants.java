57f5c87e33f297c1335afc70f1e7389e1aed7cf0
dzikoysk <dzikoysk@dzikoysk.net>
2018-01-23 17:11:21 +0100

    Remove SNAPSHOT indication

--- panda/src/main/java/org/panda_lang/panda/PandaConstants.java
+++ panda/src/main/java/org/panda_lang/panda/PandaConstants.java
@@ -18,6 +18,6 @@ package org.panda_lang.panda;
 
 public class PandaConstants {
 
-    public static final String VERSION = "indev-0.2.11-SNAPSHOT";
+    public static final String VERSION = "indev-0.2.11";
 
 }
