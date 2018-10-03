8755283fd67af5dd3115b1c61e83fc8adf691d6b
dzikoysk <dzikoysk@dzikoysk.net>
2018-10-03 21:59:59 +0200

    Bump version to indev-18.10.3

--- panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
@@ -18,6 +18,6 @@ package org.panda_lang.panda.framework;
 
 public class PandaFrameworkConstants {
 
-    public static final String VERSION = "indev-0.9.2";
+    public static final String VERSION = "indev-18.10.3";
 
 }
