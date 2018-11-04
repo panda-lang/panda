986a63adf789a9495cd75a7e5386ae5bc679a788
dzikoysk <dzikoysk@dzikoysk.net>
2018-11-04 14:10:43 +0100

    Change version to indev-18.10.17

--- panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
@@ -18,6 +18,6 @@ package org.panda_lang.panda.framework;
 
 public class PandaFrameworkConstants {
 
-    public static final String VERSION = "indev-18.10.17";
+    public static final String VERSION = "indev-18.11.4";
 
 }
