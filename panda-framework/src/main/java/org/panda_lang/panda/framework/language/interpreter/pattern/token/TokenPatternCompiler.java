acbb5fd499a41070d7f26c3e74cff7a6a6c6141c
dzikoysk <dzikoysk@dzikoysk.net>
2018-11-04 23:47:43 +0100

    Initialize the Reforged pattern

--- panda-framework/src/main/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/TokenPatternCompiler.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/TokenPatternCompiler.java
@@ -3,6 +3,8 @@ package org.panda_lang.panda.framework.language.interpreter.pattern.token;
 class TokenPatternCompiler {
 
     TokenPatternElement compile(String pattern) {
+
+
         return null;
     }
 
---
+++ panda-framework/src/test/java/org/panda_lang/panda/framework/language/interpreter/pattern/reforged/ReforgedPatternTest.java
@@ -0,0 +1,12 @@
+package org.panda_lang.panda.framework.language.interpreter.pattern.reforged;
+
+import org.junit.jupiter.api.Test;
+
+class ReforgedPatternTest {
+
+    @Test
+    public void testReforgedPattern() {
+
+    }
+
+}
