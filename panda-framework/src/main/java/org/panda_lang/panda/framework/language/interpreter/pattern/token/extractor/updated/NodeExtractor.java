e1356756d3322b33b38fe53fce8bc78d4a600e38
dzikoysk <dzikoysk@dzikoysk.net>
2018-11-23 22:35:14 +0100

    Implement NodeExtractor and part of the NodeLookupExtractor

--- panda-framework/src/main/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/extractor/updated/NodeExtractor.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/extractor/updated/NodeExtractor.java
@@ -1,17 +1,49 @@
 package org.panda_lang.panda.framework.language.interpreter.pattern.token.extractor.updated;
 
+import org.panda_lang.panda.framework.language.interpreter.pattern.lexical.elements.LexicalPatternElement;
 import org.panda_lang.panda.framework.language.interpreter.pattern.lexical.elements.LexicalPatternNode;
 import org.panda_lang.panda.framework.language.interpreter.pattern.token.TokenDistributor;
 
+import java.util.List;
+
 class NodeExtractor extends AbstractElementExtractor<LexicalPatternNode> {
+
+    private final NodeLookupExtractor nodeLookupExtractor = new NodeLookupExtractor(this);
 
     protected NodeExtractor(ExtractorWorker worker) {
         super(worker);
     }
 
     @Override
-    public ExtractorResult extract(LexicalPatternNode element, TokenDistributor distributor) {
-        return null;
+    public ExtractorResult extract(LexicalPatternNode node, TokenDistributor distributor) {
+        List<LexicalPatternElement> elements = node.getElements();
+        ExtractorResult result = new ExtractorResult();
+
+        for (int i = 0; i < elements.size(); i++) {
+            LexicalPatternElement element = elements.get(i);
+            ExtractorResult workerResult;
+
+            if (!element.isWildcard()) {
+                 workerResult = super.getWorker().extract(distributor, element);
+            }
+            else {
+                NodeLookupExtractor.LookupResult lookupResult = nodeLookupExtractor.extractNode(elements.subList(i, elements.size()), distributor);
+                workerResult = lookupResult.getMergedResults();
+                i += lookupResult.matchedIndex;
+            }
+
+            if (!workerResult.isMatched()) {
+                if (element.isOptional()) {
+                    continue;
+                }
+
+                return new ExtractorResult("Could not extract element, caused by: " + workerResult.getErrorMessage());
+            }
+
+            result.merge(workerResult);
+        }
+
+        return result;
     }
 
 }
