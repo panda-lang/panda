b82e6fbfdfd5f7c172e853835e37b3ccd1b0a92c
dzikoysk <dzikoysk@dzikoysk.net>
2018-11-23 18:19:34 +0100

    Implement WildcardExtractor

--- panda-framework/src/main/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/extractor/updated/ExtractorResult.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/extractor/updated/ExtractorResult.java
@@ -1,22 +1,27 @@
 package org.panda_lang.panda.framework.language.interpreter.pattern.token.extractor.updated;
 
+import org.panda_lang.panda.framework.design.interpreter.token.Tokens;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ExtractorResult {
 
     private final boolean matched;
-    private final String message;
+    private final String errorMessage;
     private final List<String> identifiers = new ArrayList<>();
+    private final Map<String, Tokens> wildcards = new HashMap<>();
 
     public ExtractorResult(boolean matched) {
         this.matched = matched;
-        this.message = "<unknown>";
+        this.errorMessage = "<unknown>";
     }
 
-    public ExtractorResult(String message) {
+    public ExtractorResult(String errorMessage) {
         this.matched = false;
-        this.message = message;
+        this.errorMessage = errorMessage;
     }
 
     public ExtractorResult identified(String identifier) {
@@ -24,12 +29,24 @@ public class ExtractorResult {
         return this;
     }
 
+    public ExtractorResult addWildcard(String name, Tokens wildcardContent) {
+        return null;
+    }
+
     public boolean hasErrorMessage() {
-        return message != null;
+        return errorMessage != null;
     }
 
     public boolean isMatched() {
         return matched;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public Map<String, Tokens> getWildcards() {
+        return wildcards;
     }
 
     public List<String> getIdentifiers() {
