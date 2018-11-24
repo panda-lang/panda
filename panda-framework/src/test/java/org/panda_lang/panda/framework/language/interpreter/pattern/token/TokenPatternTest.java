8198dd1da9478e4bcacd2974a9d286a3bc95a9a4
dzikoysk <dzikoysk@dzikoysk.net>
2018-11-24 01:36:36 +0100

    Improve TokenPattern unit test

--- panda-framework/src/test/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/TokenPatternTest.java
+++ panda-framework/src/test/java/org/panda_lang/panda/framework/language/interpreter/pattern/token/TokenPatternTest.java
@@ -1,45 +1,26 @@
 package org.panda_lang.panda.framework.language.interpreter.pattern.token;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.panda_lang.panda.framework.design.interpreter.token.Tokens;
-import org.panda_lang.panda.framework.language.interpreter.lexer.PandaLexer;
-import org.panda_lang.panda.framework.language.interpreter.pattern.lexical.elements.LexicalPatternElement;
-import org.panda_lang.panda.framework.language.interpreter.pattern.token.extractor.popsuted.TokenExtractorResult;
-import org.panda_lang.panda.framework.language.interpreter.source.PandaSource;
-import org.panda_lang.panda.framework.language.resource.PandaSyntax;
 
 class TokenPatternTest {
 
-    private static final String CONTENT = "method void anotherEcho(String message) {" +
-                                          "    Console.print(message);" +
-                                          "}";
-    private static final Tokens SOURCE = new PandaLexer(PandaSyntax.getInstance(), new PandaSource("Test", CONTENT)).convert();
+    @Test
+    public void testMethodPattern() {
+        TokenPatternTester.test(
+                "(method|local|hidden) [static] [<return-type>] <name> `( <*parameters> `) `{ <*body> `}",
+
+                "method void anotherEcho(String message) { Console.print(message); }",
+
+                TokenPatternTester.Wildcard.of("return-type", "void"),
+                TokenPatternTester.Wildcard.of("name", "anotherEcho"),
+                TokenPatternTester.Wildcard.of("*parameters", "Stringmessage"),
+                TokenPatternTester.Wildcard.of("*body", "Console.print(message);")
+        );
+    }
 
     @Test
-    public void testTokenPattern() {
-        TokenPattern pattern = TokenPattern.builder()
-                .compile("(method|local|hidden) [static] [<return-type>] <name> `( <*parameters> `) `{ <*body> `}")
-                .build();
+    public void testClassPattern() {
 
-        LexicalPatternElement content = pattern.getPatternContent();
-        Assertions.assertNotNull(content);
-
-        TokenExtractorResult result = pattern.extract(SOURCE);
-        Assertions.assertNotNull(result);
-
-        if (result.hasErrorMessage()) {
-            System.out.println("Error message: " + result.getErrorMessage());
-        }
-
-        Assertions.assertTrue(result.isMatched());
-        Assertions.assertNotNull(result.getWildcards());
-
-        System.out.println(result.getWildcards());
-
-        Assertions.assertEquals(2, result.getWildcards().size());
-        Assertions.assertEquals("Foo", result.getWildcards().get("name").asString());
-        Assertions.assertEquals("methodanotherEcho(Stringmessage){Console.print(message);}", result.getWildcards().get("*body").asString());
     }
 
 }
