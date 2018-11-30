2f7791a35a929778fb7db5db24ef37ba79baf896
dzikoysk <dzikoysk@dzikoysk.net>
2018-11-30 18:45:00 +0100

    Rename BooleanExpressionParser to LiteralExpressionParser and support other literals

--- panda/src/main/java/org/panda_lang/panda/framework/language/interpreter/parser/implementation/general/expression/updated/ExpressionParser.java
+++ panda/src/main/java/org/panda_lang/panda/framework/language/interpreter/parser/implementation/general/expression/updated/ExpressionParser.java
@@ -1,13 +1,19 @@
 package org.panda_lang.panda.framework.language.interpreter.parser.implementation.general.expression.updated;
 
 import org.jetbrains.annotations.Nullable;
+import org.panda_lang.panda.framework.design.architecture.prototype.ClassPrototype;
 import org.panda_lang.panda.framework.design.interpreter.parser.ParserData;
 import org.panda_lang.panda.framework.design.interpreter.token.Tokens;
 import org.panda_lang.panda.framework.design.runtime.expression.Expression;
+import org.panda_lang.panda.framework.language.architecture.value.PandaValue;
+import org.panda_lang.panda.framework.language.interpreter.parser.PandaComponents;
+import org.panda_lang.panda.framework.language.runtime.expression.PandaExpression;
 
 public interface ExpressionParser {
 
-    Expression parse(ParserData data, Tokens expressionsource);
+    @Nullable Tokens read(Tokens source);
+
+    Expression parse(ParserData data, Tokens source);
 
     default @Nullable Expression parseSilently(ParserData data, Tokens source) {
         try {
@@ -19,6 +25,13 @@ public interface ExpressionParser {
         return null;
     }
 
-    @Nullable Tokens read(Tokens source);
+    default Expression toSimpleKnownExpression(ParserData data, String className, Object value) {
+        ClassPrototype type = data.getComponent(PandaComponents.PANDA_SCRIPT).getModuleLoader().forClass(className);
+        return toSimpleKnownExpression(type, value);
+    }
+
+    default Expression toSimpleKnownExpression(ClassPrototype type, Object value) {
+        return new PandaExpression(new PandaValue(type, value));
+    }
 
 }
