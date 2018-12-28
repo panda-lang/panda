ea76791376ba61cfbc5f62d481be250637f4601c
dzikoysk <dzikoysk@dzikoysk.net>
2018-12-29 00:24:33 +0100

    Release indev-18.12.29 (Resolves #72)

--- panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
@@ -18,6 +18,6 @@ package org.panda_lang.panda.framework;
 
 public class PandaFrameworkConstants {
 
-    public static final String VERSION = "indev-18.12.19";
+    public static final String VERSION = "indev-18.12.29";
 
 }
