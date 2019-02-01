c6f8b1cb4b79a6f9a4ca1f0fc40587356926efd8
dzikoysk <dzikoysk@dzikoysk.net>
2019-02-01 17:06:34 +0100

    Release the first February version - indev-19.2.1

--- panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
@@ -18,6 +18,6 @@ package org.panda_lang.panda.framework;
 
 public class PandaFrameworkConstants {
 
-    public static final String VERSION = "indev-19.1.30";
+    public static final String VERSION = "indev-19.2.1";
 
 }
