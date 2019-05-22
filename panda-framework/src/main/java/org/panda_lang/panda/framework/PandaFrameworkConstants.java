6148aa095ee48cf7733088983eb07edeaf38ff0f
dzikoysk <dzikoysk@dzikoysk.net>
2019-05-22 21:05:27 +0200

    Bump version to indev-19.5.22

--- panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
@@ -18,6 +18,6 @@ package org.panda_lang.panda.framework;
 
 public class PandaFrameworkConstants {
 
-    public static final String VERSION = "indev-19.2.16";
+    public static final String VERSION = "indev-19.5.22";
 
 }
