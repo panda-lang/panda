fe61beed39f69a71273dc64142adab8c418c528a
dzikoysk <dzikoysk@dzikoysk.net>
2019-06-12 01:33:20 +0200

    Release indev-19.6.12 (Resolves #183, resolves #182, resolves #181, resolves #179, resolves #177)

--- panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
@@ -18,6 +18,6 @@ package org.panda_lang.panda.framework;
 
 public class PandaFrameworkConstants {
 
-    public static final String VERSION = "indev-19.6.4";
+    public static final String VERSION = "indev-19.6.12";
 
 }
