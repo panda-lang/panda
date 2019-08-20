a3190b0a34102b06df7ee5d7434413ba08225e7a
dzikoysk <dzikoysk@dzikoysk.net>
2019-08-20 16:38:31 +0200

    GH-303 Release indev-19.8.20 (Resolves #303)

--- panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/panda/framework/PandaFrameworkConstants.java
@@ -21,6 +21,6 @@ public class PandaFrameworkConstants {
     /**
      * Current version of the Panda Framework
      */
-    public static final String VERSION = "indev-19.8.15";
+    public static final String VERSION = "indev-19.8.20";
 
 }
