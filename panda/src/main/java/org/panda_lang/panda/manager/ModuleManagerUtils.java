56d1be605be55890f1049bc4d0a088f6e8b5a400
dzikoysk <dzikoysk@dzikoysk.net>
2019-12-07 13:35:07 +0100

    GH-392 Support local modules by require statement (Resolves #437)

---
+++ panda/src/main/java/org/panda_lang/panda/manager/ModuleManagerUtils.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015-2019 Dzikoysk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.panda_lang.panda.manager;
+
+import org.panda_lang.framework.design.architecture.Environment;
+import org.panda_lang.framework.design.interpreter.source.Source;
+import org.panda_lang.framework.language.interpreter.source.PandaURLSource;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Objects;
+
+public final class ModuleManagerUtils {
+
+    private ModuleManagerUtils() { }
+
+    /**
+     * Load module
+     *
+     * @param environment the environment
+     * @param moduleDirectory the module directory to load
+     * @throws IOException when module directory or module file does not exist
+     */
+    public static void loadToEnvironment(Environment environment, File moduleDirectory) throws IOException {
+        ModuleDocument moduleInfo = new ModuleDocumentFile(new File(moduleDirectory, "panda.hjson")).getContent();
+
+        environment.getModulePath().include(moduleDirectory.getName(), () -> {
+            Source source = PandaURLSource.fromFile(new File(moduleDirectory, Objects.requireNonNull(moduleInfo.getMainScript())));
+            environment.getInterpreter().interpret(source);
+        });
+    }
+
+}
