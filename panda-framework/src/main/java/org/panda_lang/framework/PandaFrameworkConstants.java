2a7f6e8bbf89b09c9e735b620b909b7ec3940d53
dzikoysk <dzikoysk@dzikoysk.net>
2019-12-12 17:28:08 +0100

    GH-392 Bump Panda version to indev-19.12.12

--- panda-framework/src/main/java/org/panda_lang/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/framework/PandaFrameworkConstants.java
@@ -21,7 +21,7 @@ public final class PandaFrameworkConstants {
     /**
      * Current version of the Panda Framework
      */
-    public static final String VERSION = "indev-19.11.19";
+    public static final String VERSION = "indev-19.12.12";
 
     private PandaFrameworkConstants() { }
 
