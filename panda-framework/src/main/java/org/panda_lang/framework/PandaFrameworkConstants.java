9a92a107a9ec9f1cf998243fe00fa228a223d2ed
dzikoysk <dzikoysk@dzikoysk.net>
2020-04-05 16:32:44 +0200

    GH-492 Release indev-20.4.5

--- panda-framework/src/main/java/org/panda_lang/framework/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/framework/PandaFrameworkConstants.java
@@ -21,7 +21,7 @@ public final class PandaFrameworkConstants {
     /**
      * Current version of the Panda Framework
      */
-    public static final String VERSION = "indev-20.4.2";
+    public static final String VERSION = "indev-20.4.5";
 
     private PandaFrameworkConstants() { }
 
