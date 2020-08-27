e2ee1ba71e9b78c6794e9613c3f2083adb5490f3
Takuma Watanabe <takuma.watanabe@gmail.com>
2020-08-27 18:10:30 +0900

    Add missing tests for Panda Utilities (#557)
    * Add tests for org.panda_lang.utilities.commons.iterable.ArrayIterable

---
+++ panda-utilities/src/test/java/org/panda_lang/utilities/commons/iterable/ArrayIterableTest.java
@@ -0,0 +1,26 @@
+package org.panda_lang.utilities.commons.iterable;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Iterator;
+
+class ArrayIterableTest {
+
+    @Test
+    void testArrayIterable() {
+        ArrayIterable<String> arrayIterable =
+                new ArrayIterable<>(new String[]{"a", "b", "c"});
+
+
+        Iterator<String> iterator = arrayIterable.iterator();
+
+
+        Assertions.assertTrue(iterator.hasNext());
+        Assertions.assertEquals("a", iterator.next());
+        Assertions.assertEquals("b", iterator.next());
+        Assertions.assertEquals("c", iterator.next());
+        Assertions.assertFalse(iterator.hasNext());
+    }
+
+}
