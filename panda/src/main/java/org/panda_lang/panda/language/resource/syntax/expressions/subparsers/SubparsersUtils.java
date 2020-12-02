8a1d7fb74e199fd5217cc8e1cccfe2e17ebdd451
dzikoysk <dzikoysk@dzikoysk.net>
2020-12-02 02:28:17 +0100

    GH-556 Change synchronized-source instead of root source-stream in  SubparsersUtils#readType utility method

--- panda/src/main/java/org/panda_lang/panda/language/resource/syntax/expressions/subparsers/SubparsersUtils.java
+++ panda/src/main/java/org/panda_lang/panda/language/resource/syntax/expressions/subparsers/SubparsersUtils.java
@@ -20,7 +20,6 @@ import org.jetbrains.annotations.Nullable;
 import org.panda_lang.language.architecture.type.signature.Signature;
 import org.panda_lang.language.interpreter.parser.expression.ExpressionContext;
 import org.panda_lang.language.interpreter.parser.expression.ExpressionResult;
-import org.panda_lang.language.interpreter.token.SourceStream;
 import org.panda_lang.panda.language.interpreter.parser.PandaSourceReader;
 import org.panda_lang.panda.language.resource.syntax.type.SignatureParser;
 import org.panda_lang.utilities.commons.function.Result;
@@ -32,14 +31,12 @@ final class SubparsersUtils {
     private SubparsersUtils() { }
 
     protected static Result<Signature, ExpressionResult> readType(@Nullable Signature parent, ExpressionContext<?> context) {
-        SourceStream stream = context.toStream();
-        PandaSourceReader sourceReader = new PandaSourceReader(context.toStream());
-        int index = stream.getUnreadLength();
+        PandaSourceReader sourceReader = new PandaSourceReader(context.getSynchronizedSource().getAvailableSource().toStream());
 
         return sourceReader.readSignature()
                 .map(source -> Result.<Signature, ExpressionResult> ok(SIGNATURE_PARSER.parse(parent, context, source)))
                 .orElseGet(() -> Result.error(ExpressionResult.error("Unknown type", context.getSynchronizedSource().getSource())))
-                .peek(signature -> context.getSynchronizedSource().next(index - stream.getUnreadLength()));
+                .peek(signature -> context.getSynchronizedSource().next(sourceReader.getStream().getReadLength()));
     }
 
 }
