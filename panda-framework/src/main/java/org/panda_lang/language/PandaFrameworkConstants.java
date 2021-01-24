4214774e18db59f7e8d0e29b76cc1af80be85cae
dzikoysk <dzikoysk@dzikoysk.net>
2021-01-24 14:30:00 +0100

    Release 0.3.0-alpha

--- panda-framework/src/main/java/org/panda_lang/language/PandaFrameworkConstants.java
+++ panda-framework/src/main/java/org/panda_lang/language/PandaFrameworkConstants.java
@@ -21,7 +21,7 @@ public final class PandaFrameworkConstants {
     /**
      * Current version of the Panda Framework
      */
-    public static final String VERSION = "0.2.1-alpha";
+    public static final String VERSION = "0.3.0-alpha";
 
     private PandaFrameworkConstants() { }
 
